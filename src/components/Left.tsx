5c26c270629eb98284440b267bfb69f0cca61c7f
pnewsam <paul.newsam@gmail.com>
2023-10-21 11:56:59 -0700

    Adds downloads page with resume download.

---
+++ src/components/Left.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export const Left = ({ children }: { children: ReactNode }) => (
+  <div className="pt-16 pb-8 md:py-32 px-8 lg:px-16 border-b md:border-r border-stone-300 bg-stone-100">
+    <div className="w-full">{children}</div>
+  </div>
+);
