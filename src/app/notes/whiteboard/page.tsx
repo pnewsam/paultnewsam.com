e4adc5756c62d36d5c25043f555db88172c8afbe
pnewsam <paul.newsam@gmail.com>
2024-11-27 17:32:43 -0800

    Fix error

--- src/app/notes/whiteboard/page.tsx
+++ src/app/notes/whiteboard/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { Brush } from "lucide-react";
 import Container from "@/components/Container";
+import { Body } from "@/components/typography/Body";
 import { setupCanvas } from "./_scripts/setupCanvas";
 import initializers from "./_scripts/initializers";
 import Card from "@/components/Card";
@@ -21,11 +22,10 @@ export default function WhiteboardPage() {
   return (
     <Container>
       <section className="py-12 md:py-24">
-        <PageHeader
-          title="Whiteboard"
-          tags={["HTML", "Typescript"]}
-          description="A basic whiteboard using HTML <canvas />. Click and drag to draw."
-        />
+        <PageHeader title="Whiteboard" tags={["HTML", "Typescript"]} />
+        <Body>
+          A basic whiteboard using HTML <canvas />. Click and drag to draw.
+        </Body>
         <Card className="relative">
           <span className="absolute top-0 right-0 text-base uppercase tracking-widest font-medium p-4 text-neutral-500 dark:text-neutral-400 flex items-center gap-2 bg-neutral-100 dark:bg-neutral-800 rounded-tr-md rounded-bl-md border-t border-r border-neutral-200 dark:border-neutral-800">
             Canvas
